44bfee8051743582af8584bc024f6b3f35e06e80
PANYUE WEI <1306730827@qq.com>
2026-03-09 22:23:02 +0800

    Time: 0 ms (100%), Space: 11.7 MB (21.6%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n= nums.size();
+        int left=0,right=n-1;
+        if(n==0) return 0;
+        else if(n==1) return nums[0]==val?0:1;
+        while(left<right){
+            int tmp;
+            while(left<n && nums[left]!=val) left++;
+            while(right>-1 && nums[right]==val) right--;
+            if(left>=n) return n;
+            if(right<=-1) return 0;
+            // cout << left << right << endl;
+            if(left<right){
+                tmp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=tmp;
+            }
+            else break;
+            
+        }
+        // for(int x:nums){
+        //     cout << x;
+        // }
+        cout << left << right << endl;
+        return left;
+    }
+   
+};
